34bd429c7b4729786e29f8e2bd259e823b69a2e1
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-01-21 18:29:01 +0100

    Update webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,6 @@
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
 import "dotenv/config";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 import * as path from "path";
 import * as webpack from "webpack";
 
@@ -8,11 +9,11 @@ const environment = process.env.NODE_ENV === "development" ? "development" : "pr
 const config: webpack.Configuration = {
   mode: environment,
   entry: {
-    main: path.resolve(__dirname, "./src/App.ts"),
-    html: path.resolve(__dirname, "./src/index.html")
+    main: path.resolve(__dirname, "./src/App.ts")
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({template: "index.html"}),
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
@@ -26,14 +27,17 @@ const config: webpack.Configuration = {
       }, {
         test: /\.(scss|css)$/,
         use: ["style-loader", "css-loader", "sass-loader"]
-      },  {
+      }, {
         test: /\.html$/i,
         loader: "html-loader"
-      },{
+      }, {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: "asset/resource"
       }
     ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -41,7 +45,7 @@ const config: webpack.Configuration = {
   },
   devServer: {
     publicPath: "/",
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, "."),
     compress: true,
     hot: true,
     port: 8080
