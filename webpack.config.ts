cf484bc82b71fe65dda846b02097407d4594ad7f
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-01-21 09:16:18 +0100

    Add webpack server configuration

---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import * as path from "path";
+import * as webpack from "webpack";
+
+const environment = process.env.NODE_ENV === "development" ? "development" : "production";
+
+const config: webpack.Configuration = {
+  mode: environment,
+  entry: "./src/App.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader"
+        }
+      }, {
+        test: /\.scss$/,
+        use: {
+          loader: "sass-loader"
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "App.js"
+  },
+  devtool: "source-map"
+};
+
+export default config;
