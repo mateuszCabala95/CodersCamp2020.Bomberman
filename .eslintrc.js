d0f8568841621bede6169240525de15880a7c186
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-01-22 17:51:15 +0100

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
+    jest: true,
+    node: true
   },
   extends: [
     "eslint:recommended",
@@ -13,11 +15,41 @@ module.exports = {
      ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 12
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      arrowFunctions: true,
+    },
   },
   plugins: [
-    '@typescript-eslint'
+    '@typescript-eslint',
+    "prettier"
   ],
   rules: {
-  }
+    "no-extra-semi": "error",
+    "no-console": "warn",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      rules: {
+        "@typescript-eslint/explicit-function-return-type": [
+          "warn",
+          {
+            allowExpressions: true,
+          },
+        ],
+      },
+    },
+    {
+      files: ["**/*.js", "**/*.jsx"],
+      plugins: ["@typescript-eslint"],
+      rules: {
+        "@typescript-eslint/explicit-function-return-type": 0,
+      },
+    },
+  ],
 }
