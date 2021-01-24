b4f1ada561cc7df506aff0e8cb1c16e43d950ebd
Karolina Gutka <karolina.gutka.97@gmail.com>
2021-01-24 02:08:46 +0100

    Create game loop and drawing grid

---
+++ src/game/game.ts
@@ -0,0 +1,52 @@
+import { Entity} from "../utils";
+import { Settings } from "../settings";
+import { Grid } from "../grid";
+
+export class Game extends Entity {
+    private _lastTimestamp = 0
+
+    private _entities: Entity[] = []
+
+    public get Entities(): Entity[] {
+        return this._entities
+    }
+
+    public Awake(): void {
+        super.Awake()
+
+        // instantiate and Grid to the list of children
+        this._entities.push(new Grid())
+
+        // awake all children
+        for (const entity of this.Entities) {
+            entity.Awake()
+        }
+
+        // Make sure Update starts after all entities are awaken
+        window.requestAnimationFrame(() => {
+            // set initial timestamp
+            this._lastTimestamp = Date.now()
+
+            // start update loop
+            this.Update()
+        })
+    }
+
+    public Update(): void {
+        const deltaTime = (Date.now() - this._lastTimestamp) / 1000
+
+        // update all components
+        super.Update(deltaTime)
+
+        // update all children
+        for (const entity of this.Entities) {
+            entity.Update(deltaTime)
+        }
+
+        // update the timestamp
+        this._lastTimestamp = Date.now()
+
+        // Invoke on next frame
+        window.requestAnimationFrame(() => this.Update())
+    }
+}
---
+++ src/settings/settings.ts
@@ -0,0 +1,11 @@
+import { Color } from "../utils";
+
+export const Settings = Object.freeze({
+    grid: {
+        dimension: 11,
+        nodeSize: 100,
+        nodeOffset: 10,
+        color: new Color(245, 245, 245, 1)
+    }
+})
+
