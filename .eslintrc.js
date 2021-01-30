ef1afa7b7cb36787f636f9068f1cb77cc7274cee
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-01-30 14:59:07 +0100

    Open Pull Request
    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:react/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
