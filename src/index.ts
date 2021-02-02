dbe3500abd316c423b2c417ba2a239d5fc0e8784
adam-kostuch <adamkostuch.krakow@gmail.com>
2021-02-02 18:51:29 +0100

    Changing and testing isMobileDevice func

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import MobileDetectionService from "./services/MobileDetection"
 const joy = new Joystick("joyDiv")
 const mobile = provider.get<MobileDetectionService>("mobileDetection")
 joy.render()
+alert("Is mobile device: " + mobile.isMobileDevice())
 mobile.logEvent()
 
 export default app
