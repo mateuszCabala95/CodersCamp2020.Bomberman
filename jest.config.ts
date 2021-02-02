e4b01bd80a9c9b2982471de80431f06626ace2b0
adam-kostuch <adamkostuch.krakow@gmail.com>
2021-02-02 16:44:54 +0100

    Reconfigurating jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,25 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jsdom"
+  testEnvironment: "jsdom",
+}
+
+export default {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+
+  moduleFileExtensions: ["js", "ts"],
+
+  restoreMocks: true,
+
+  roots: ["<rootDir>/src"],
+
+  testEnvironment: "jsdom",
+
+  testMatch: ["**/*.spec.(js|ts)"],
+
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
 }
 
 export default {
