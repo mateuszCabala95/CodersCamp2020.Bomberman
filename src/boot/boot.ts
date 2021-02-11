6ce1aefc945d433122ecf4ac471e017918d284d0
Kacper <kacperzolkiewski@interia.pl>
2021-02-11 12:32:45 +0100

    Add basic movement to boots

--- src/boot/boot.ts
+++ src/boot/boot.ts
@@ -10,21 +10,30 @@ export class Boot extends Entity {
   private readonly _grid: Grid
   private _currentNodeIdx: number
 
-  public Move(x: number, y: number): void {
-    const currentPos = this._currentNodeIdx
-    const dim = Settings.grid.dimension
-    const currX = currentPos % dim
-    const currY = Math.floor(currentPos / dim)
+  public Move(): void {
+    setInterval(() => {
+      const move = [
+        { x: 0, y: 1 },
+        { x: 0, y: -1 },
+        { x: 1, y: 0 },
+        { x: -1, y: 0 },
+      ]
+      const rand = Math.floor(Math.random() * 4)
+      const currentPos = this._currentNodeIdx
+      const dim = Settings.grid.dimension
+      const currX = currentPos % dim
+      const currY = Math.floor(currentPos / dim)
 
-    if (currY === 0 && y === -1) return
-    if (currY === dim - 1 && y === 1) return
-    if (currX === 0 && x === -1) return
-    if (currX === dim - 1 && x === 1) return
+      if (currY === 0 && move[rand].y === -1) return
+      if (currY === dim - 1 && move[rand].y === 1) return
+      if (currX === 0 && move[rand].x === -1) return
+      if (currX === dim - 1 && move[rand].x === 1) return
 
-    // validate if player can move there (walls) - TODO
-    this._currentNodeIdx = currentPos + x + y * dim
-    const nextNode = this._grid.Nodes[this._currentNodeIdx]
-    this._locomotionComponent.Node = nextNode
+      // validate if player can move there (walls) - TODO
+      this._currentNodeIdx = currentPos + move[rand].x + move[rand].y * dim
+      const nextNode = this._grid.Nodes[this._currentNodeIdx]
+      this._locomotionComponent.Node = nextNode
+    }, 1000)
   }
 
   public get Position(): Vector2D | null {
@@ -45,6 +54,7 @@ export class Boot extends Entity {
     this.AddComponent(new BootDrawComponent(this))
 
     super.Awake()
+    this.Move()
   }
 
   public Update(deltaTime: number): void {
