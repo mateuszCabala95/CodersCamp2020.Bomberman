d92bbbdcfab50b382ec736265d00398fcaea3162
adam-kostuch <73581427+adam-kostuch@users.noreply.github.com>
2021-02-14 20:09:43 +0100

    Delete team.js

--- src/team/team.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-exports.__esModule = true
-exports.Team = void 0
-var Team
-;(function (Team) {
-  Team[(Team["A"] = 0)] = "A"
-  Team[(Team["B"] = 1)] = "B"
-})((Team = exports.Team || (exports.Team = {})))
