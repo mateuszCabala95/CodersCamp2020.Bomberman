0dc40c9e16599c8e564a5097ecff1811bd8032e5
Rafał Litwinowicz <rafal.litwinowicz@gmail.com>
2021-02-14 18:16:36 +0100

    added avatars, cleanup

--- src/game/game.ts
+++ src/game/game.ts
@@ -4,7 +4,6 @@ import { Player } from "../Player"
 import { Boot } from "../boot"
 import { Team } from "../team"
 import { GameInputComponent } from "./components"
-import MainPage from "../../src/pages/mainPage/MainPage"
 
 export class Game extends Entity {
   private _lastTimestamp = 0
